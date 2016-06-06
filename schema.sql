dca45b3c0c37f5bba5f8edd48ec929f70a433bdd
Jonathan Krone <jkrone@vt.edu>
2016-06-06 18:47:14 -0500

    (feat) Expand schema description. Uncertain how to handle images; will likely use an external service such as AWS (though I have no experience) or a standard image uploader.

--- schema.sql
+++ schema.sql
@@ -12,3 +12,44 @@ CREATE TABLE IF NOT EXISTS BirdsEye.customers(
 	phone integer,
 	aspirations text[]
 );
+
+CREATE TABLE IF NOT EXISTS BirdsEye.homes(
+	home_id PRIMARY KEY,
+	address varchar(150), /* unique? */
+	sqft integer,
+	stories integer,
+	bath_count integer,
+	bedroom_count integer,
+	HV_type varchar(50),
+	HV_install_date date,
+	AC_type varchar(50),
+	AC_install_date date,
+);
+
+/*
+*		I imagine a many:many customer:home relationship.
+* 		- Homes may change owners
+*			- Different members of the same home may comission consultations
+*/
+CREATE TABLE IF NOT EXISTS BirdsEye.customer_homes(
+	FOREIGN KEY home_id REFERENCES BirdsEye.homes,
+	FOREIGN KEY customer_id REFERENCES BirdsEye.customers
+);
+
+CREATE TABLE IF NOT EXISTS BirdsEye.rooms(
+	room_id PRIMARY KEY,
+	type varchar(50),
+	sqft integer,
+	window_count integer DEFAULT 0,
+	story integer,
+	FOREIGN KEY home_id REFERENCES BirdsEye.homes NOT NULL
+);
+
+/* store photos locally or submit to aws/imgur/etc? */
+CREATE TABLE IF NOT EXISTS BirdsEye.images(
+	image_id PRIMARY KEY,
+	image_url NOT NULL,
+	FOREIGN KEY customer_id REFERENCES BirdsEye.customers NOT NULL,
+	FOREIGN KEY home_id REFERENCES BirdsEye.homes,
+	FOREIGN KEY room_id RFERENCES BirdsEye.rooms, 
+);
