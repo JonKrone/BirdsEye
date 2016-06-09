91072a126ee99593d5c235296e2b869f252c2aa6
Jonathan Krone <jkrone@vt.edu>
2016-06-08 21:40:32 -0500

    (feat) whoops, missed the server-helper file.

---
+++ server/server-helper.js
@@ -0,0 +1,26 @@
+const curry = require('ramda').curry;
+const helper = module.exports;
+
+// Hoist global variables for easy referencing
+global.__lib = __dirname + '/../lib/';
+
+// Return the first element of a collection. Frequently useful for DB models.
+helper.first = (array) => array[0];
+
+/*
+  Curried helper to log a description when invoked with a
+  second parameter. Useful for locating errors. Note that
+  it re-throws the error, continuing the error chain.
+
+  example:
+	  asyncPromise
+	    .catch(reportError('creating customer'))
+	    .catch(handleError);
+*/
+helper.reportError = curry(function(description, error) {
+  console.error('\n*** Error ***');
+  console.error('***', description);
+  console.error('***', error, '\n');
+
+  if (error instanceof Error) throw error;
+});
