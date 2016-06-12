693ef93600a011e4a08ddde2fc9ab9863374c734
Jonathan Krone <jkrone@vt.edu>
2016-06-12 01:23:32 -0500

    (feat) Set NODE_ENV to test when testing

--- test/test-init.js
+++ test/test-init.js
@@ -1,5 +1,11 @@
 /*
 	This file injects TEST_HELPER, a file path, into the global namespace so that we can
 	import helpers in a consistent and explicit way.
+
+	Additionally, prepare our node environment for testing (just set a flag). This helps
+	us avoid overwriting development database information and lowers cost of instantiating
+	our server.
 */
 global.TEST_HELPER = __dirname + '/test-helper.js';
+
+process.env.NODE_ENV = 'test';
