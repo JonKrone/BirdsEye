edc3065daa388f0d71eb2e8a7c97b78d7ab09c59
Jonathan Krone <jkrone@vt.edu>
2016-06-14 23:19:17 -0500

    (feat) Just a smidge of non-response css styling. Also, Notes model :)

---
+++ server/models/notes.js
@@ -0,0 +1,31 @@
+const Help = require('../server-helper');
+
+const db = require(__lib + 'db/connection');
+const Notes = module.exports;
+
+/*
+	Create a note. Notes have a tier of ownership: customer, home, room.
+
+	@param note {
+		content: <String>,
+		author: <Number> user who wrote note,
+		customer_id: <Number> customer related to note,
+		home_id: <Number, optional> home related to note,
+		room_id: <Number, optional> room related to note,
+	}
+*/
+Notes.create = function(note) {
+	return db('notes')
+		.returning('note_id')
+		.insert(note)
+		.then(Help.first)
+		.catch(Help.reportError('Creating note'));
+};
+
+// Fetch all the notes of customer @param _customer_id
+Notes.ofCustomer = function(_customer_id) {
+	return db('notes')
+		.where({ customer_id: _customer_id })
+		.catch(Help.reportError('Retrieving notes of a customer'));
+}
+
